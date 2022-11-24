e9a39c38beeb90a8cb4b454127faaae9c697978b
Elena1011 <116499734+Elena1011@users.noreply.github.com>
2022-11-24 23:22:22 +0300

    Домашка со 2,4,6,8

---
+++ Desktop/HomeWORK1/Task04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.WriteLine("Введите 3 числа: ");
+
+int a, b, c;
+
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine($"Наибольше число: {max}");
+
